aa5f45a3ced85594a860ca991da72304e7fc6319
Paige Miles <paige@lucas-miles.com>
2020-10-28 21:01:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftString = dodger.style.left.replace("px", "");
+    let leftNumber = parseInt(leftString, 10);
+    if (leftNumber > 0) {
+        dodger.style.left = `${leftNumber - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftString = dodger.style.left.replace("px", "");
+    let leftNumber = parseInt(leftString, 10);
+    if (leftNumber > 0) {
+        dodger.style.left = `${leftNumber + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
